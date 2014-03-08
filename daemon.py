e87ecfa94c03cdc3df589f6b42fbb5587dec43de
Michał Zieliński <michal@zielinscy.org.pl>
2014-03-08 13:13:17 +0100

    first commit

---
+++ daemon.py
@@ -0,0 +1,112 @@
+import os
+import sys
+import pwd
+import subprocess
+import threading
+import json
+import socket
+import pipes
+import time
+
+ENVIRON = {'PATH': '/bin:/usr/bin'}
+config = json.load(open('config.json'))
+
+open_session_lock = threading.Lock()
+
+def auth_helper(username, token):
+    cmd = ['ssh',
+           '-o', 'StrictHostKeyChecking=no',
+           '-o', 'PubkeyAuthentication=no',
+           '-l' + username,
+           config['ssh_host'], 'true']
+    return run_ssh(cmd, token)
+
+def mount_helper(username, token, pw):
+    cmd = ['sshfs', '-f',
+           '-o', 'reconnect,workaround=all',
+           '-o', 'nonempty,allow_other',
+           '-o', 'compression=yes',
+           '-o', 'uid=%d,gid=%d' % (
+               pw.pw_uid, pw.pw_gid),
+           '%s@%s:' % (username, config['ssh_host']),
+           pw.pw_dir]
+    return run_ssh(cmd, token)
+
+def run_ssh(cmd, token):
+    ipipe, opipe = os.pipe()
+    cmd = ['sshpass', '-d%d' % ipipe, '--'] + cmd
+    proc = subprocess.Popen(cmd,
+                            env=ENVIRON,
+                            close_fds=False,
+                            stdout=sys.stderr)
+    os.write(opipe, token + '\n')
+    os.close(opipe)
+    return proc.wait()
+
+def open_session(user, auth_token):
+    pw = pwd.getpwnam(user)
+    path = pw.pw_dir
+
+    try:
+        os.mkdir(path)
+    except OSError:
+        pass
+    else:
+        os.chown(path, pw.pw_uid, pw.pw_gid)
+
+    if not ismount(path):
+        f = lambda: mount_helper(user, auth_token, pw)
+        threading.Thread(target=f).start()
+        wait_for_mount(path)
+        subprocess.check_call([
+            'mount', '-t', 'tmpfs', 'cache',
+            path + '/.cache'
+        ])
+        return 0
+    else:
+        return 0
+
+def wait_for_mount(path, timeout=5):
+    for i in xrange(10 * timeout):
+        if ismount(path):
+            return
+        time.sleep(0.1)
+    raise IOError('not mounted')
+
+def ismount(path):
+    for line in open('/proc/mounts'):
+        if line.split()[1] == path:
+            return True
+
+def auth(user, auth_token):
+    return auth_helper(user, auth_token)
+
+def handle(sock):
+    f = sock.makefile('r+')
+    method, args = json.loads(f.readline())
+    code = -1
+
+    if method == 'open_session':
+        with open_session_lock:
+            code = open_session(**args)
+    elif method == 'auth':
+        code = auth(**args)
+
+    print 'response', method, code
+    f.write(str(code) + '\n')
+
+def loop():
+    ADDR = 'sock'
+    if os.path.exists(ADDR):
+        os.remove(ADDR)
+    sock = socket.socket(socket.AF_UNIX)
+    sock.bind(ADDR)
+    os.chmod(ADDR, 0o700)
+    sock.listen(2)
+    while True:
+        child, addr = sock.accept()
+        threading.Thread(target=handle, args=[child]).start()
+        del child
+
+if __name__ == '__main__':
+    loop()
---
+++ pam_ssh.py
@@ -0,0 +1,67 @@
+import os
+import sys
+import pwd
+import socket
+import json
+
+auth_token = None
+
+def rpc(name, args):
+    sock = socket.socket(socket.AF_UNIX)
+    sock.connect('/home/michal/pam_ssh/sock')
+    f = sock.makefile('r+')
+    f.write(json.dumps([name, args]) + '\n')
+    f.flush()
+    resp = int(f.readline())
+    return resp
+
+def pam_sm_authenticate(pamh, flags, argv):
+    global auth_token
+
+    username = pamh.get_user()
+    pw = pwd.getpwnam(username)
+    if pw.pw_uid < 1000:
+        return pamh.PAM_AUTH_ERR
+
+    assert username == 'zlmch'
+    auth_token = pamh.authtok
+    if len(auth_token) > 1024:
+        return pamh.PAM_AUTH_ERR
+
+    if not auth_token:
+        return pamh.PAM_AUTH_ERR
+
+    code = rpc('auth', dict(user=username, auth_token=auth_token))
+
+    if code == 0:
+        return pamh.PAM_SUCCESS
+    else:
+        return pamh.PAM_AUTH_ERR
+
+def pam_sm_setcred(pamh, flags, argv):
+    return pamh.PAM_SUCCESS
+
+def pam_sm_acct_mgmt(pamh, flags, argv):
+    return pamh.PAM_SUCCESS
+
+def pam_sm_open_session(pamh, flags, argv):
+    user = pamh.get_user()
+    pw = pwd.getpwnam(user)
+    token = auth_token
+
+    if pw.pw_uid < 1000:
+        return pamh.PAM_SUCCESS
+
+    code = rpc('open_session', dict(user=user,
+                                    auth_token=auth_token))
+
+    if code == 0:
+        return pamh.PAM_SUCCESS
+    else:
+        return pamh.PAM_AUTH_ERR
+
+def pam_sm_close_session(pamh, flags, argv):
+    return pamh.PAM_SUCCESS
+
+def pam_sm_chauthtok(pamh, flags, argv):
+    return pamh.PAM_SUCCESS
